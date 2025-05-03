b8ede56cec44b1f34651451be33c9b89617a55ce
Lucas Augusto Kepler <lucaskepler991@gmail.com>
2025-05-03 14:04:36 -0300

    add delete dialog

--- src/pages/trainings/trainings.tsx
+++ src/pages/trainings/trainings.tsx
@@ -17,7 +17,27 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { Breadcrumb } from "@/components/app-breadcrumb/app-breadcrumb";
-import { Pen, Trash } from "lucide-react";
+import { Pen, Trash, Eye } from "lucide-react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 interface Training {
   id: number;
@@ -29,11 +49,20 @@ interface Training {
   created_at: string;
   updated_at: string;
   categoria_id: number;
+  categoria_nome: string;
   secao_id: number;
 }
 
 export default function Trainings() {
-  const [trainings, setTrainings] = useState<Training[]>([]); // Estado para armazenar os treinamentos
+  const [trainings, setTrainings] = useState<Training[]>([]);
+  const [categories, setCategories] = useState<
+    { id: number; titulo: string }[]
+  >([]);
+  const [selectedTraining, setSelectedTraining] = useState<Training | null>(
+    null
+  );
+  const [isDialogOpen, setIsDialogOpen] = useState(false); // Estado para controlar o Dialog
+
   const breadcrumbItems = [
     { label: "Home", href: "/" },
     { label: "Treinamentos", href: "/trainings" },
@@ -46,13 +75,13 @@ export default function Trainings() {
           "http://127.0.0.1:8000/trainings/trainings",
           {
             method: "GET",
-            credentials: "include", // Inclui cookies automaticamente
+            credentials: "include",
           }
         );
 
         if (response.ok) {
           const result = await response.json();
-          setTrainings(result.success || []); // Supondo que os treinamentos estão no campo "success"
+          setTrainings(result.success || []);
         } else {
           console.error("Erro ao carregar os treinamentos.");
         }
@@ -64,9 +93,56 @@ export default function Trainings() {
     fetchTrainings();
   }, []);
 
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch(
+          "http://127.0.0.1:8000/categories/categories",
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        );
+
+        if (response.ok) {
+          const result = await response.json();
+          setCategories(result.success || []);
+        } else {
+          console.error("Erro ao carregar as categorias.");
+        }
+      } catch (error) {
+        console.error("Erro ao conectar ao servidor.", error);
+      }
+    };
+    console.log(categories);
+    fetchCategories();
+  }, []);
+
+  const handleDelete = async (trainingId: number) => {
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:8000/trainings/delete/${trainingId}`,
+        {
+          method: "DELETE",
+          credentials: "include", // Inclui cookies automaticamente
+        }
+      );
+      if (response.ok) {
+        setTrainings((prev) =>
+          prev.filter((training) => training.id !== trainingId)
+        );
+        toast.success("Treinamento deletado com sucesso!");
+      } else {
+        toast.error("Erro ao excluir o treinamento.");
+      }
+    } catch (error) {
+      toast.error("Erro ao conectar ao servidor.");
+    }
+  };
+
   return (
     <Layout>
-      <Breadcrumb items={breadcrumbItems} />
+      <Breadcrumb items={[{ label: "Home", href: "/" }, { label: "Treinamentos", href: "/trainings" }]} />
       <h1 className="text-xl font-bold mt-5">Treinamentos</h1>
       <div className="flex flex-col gap-4">
         <div className="flex justify-end">
@@ -81,12 +157,12 @@ export default function Trainings() {
           <TableCaption>Treinamentos</TableCaption>
           <TableHeader>
             <TableRow>
-              <TableHead className="w-1/5">Título</TableHead>
-              <TableHead className="w-1/5">Descrição</TableHead>
-              <TableHead className="w-1/5">Criado em</TableHead>
-              <TableHead className="w-1/5">Atualizado em</TableHead>{" "}
-              <TableHead className="w-1/5">Arquivo</TableHead>
-              {/* Nova coluna */}
+              <TableHead className="w-1/6">Título</TableHead>
+              <TableHead className="w-1/6">Categoria</TableHead>
+              <TableHead className="w-1/6">Descrição</TableHead>
+              <TableHead className="w-1/6">Criado em</TableHead>
+              <TableHead className="w-1/6">Atualizado em</TableHead>
+              <TableHead className="w-1/6">Arquivo</TableHead>
               <TableHead>Ações</TableHead>
             </TableRow>
           </TableHeader>
@@ -94,18 +170,11 @@ export default function Trainings() {
             {trainings.length > 0 ? (
               trainings.map((training) => (
                 <TableRow key={training.id}>
-                  <TableCell className="font-medium">
-                    {training.titulo}
-                  </TableCell>
+                  <TableCell className="font-medium">{training.titulo}</TableCell>
+                  <TableCell className="font-medium">{training.categoria_nome}</TableCell>
                   <TableCell>{training.descricao}</TableCell>
-                  <TableCell>
-                    {new Date(training.updated_at).toLocaleString()}{" "}
-                    {/* Exibe o updated_at */}
-                  </TableCell>
-
-                  <TableCell>
-                    {new Date(training.created_at).toLocaleString()}
-                  </TableCell>
+                  <TableCell>{new Date(training.created_at).toLocaleString()}</TableCell>
+                  <TableCell>{new Date(training.updated_at).toLocaleString()}</TableCell>
                   <TableCell>
                     <a
                       href={training.arquivo_caminho}
@@ -116,7 +185,6 @@ export default function Trainings() {
                       {training.arquivo_nome}
                     </a>
                   </TableCell>
-
                   <TableCell className="flex items-center gap-2">
                     <div className="flex gap-5 items-center justify-center">
                       <TooltipProvider>
@@ -136,16 +204,44 @@ export default function Trainings() {
                       <TooltipProvider>
                         <Tooltip>
                           <TooltipTrigger asChild>
-                            <button
-                              className="hover:bg-red-300 rounded-md p-2"
-                              onClick={() =>
-                                console.log(
-                                  `Excluir treinamento ${training.id}`
-                                )
-                              }
-                            >
-                              <Trash color="red" />
-                            </button>
+                            <AlertDialog>
+                              <AlertDialogTrigger asChild>
+                                <button
+                                  className="hover:bg-red-300 rounded-md p-2"
+                                  onClick={() => setSelectedTraining(training)}
+                                >
+                                  <Trash color="red" />
+                                </button>
+                              </AlertDialogTrigger>
+                              <AlertDialogContent>
+                                <AlertDialogHeader>
+                                  <AlertDialogTitle>
+                                    Confirmar Exclusão
+                                  </AlertDialogTitle>
+                                </AlertDialogHeader>
+                                <p>
+                                  Tem certeza de que deseja excluir o treinamento{" "}
+                                  <strong>{selectedTraining?.titulo}</strong>? Essa ação não pode
+                                  ser desfeita.
+                                </p>
+                                <AlertDialogFooter>
+                                  <AlertDialogCancel className="cursor-pointer">
+                                    Cancelar
+                                  </AlertDialogCancel>
+                                  <AlertDialogAction
+                                    className="bg-red-500 cursor-pointer hover:bg-red-600"
+                                    onClick={() => {
+                                      if (selectedTraining) {
+                                        handleDelete(selectedTraining.id);
+                                        setSelectedTraining(null);
+                                      }
+                                    }}
+                                  >
+                                    Excluir
+                                  </AlertDialogAction>
+                                </AlertDialogFooter>
+                              </AlertDialogContent>
+                            </AlertDialog>
                           </TooltipTrigger>
                           <TooltipContent>
                             <p>Excluir</p>
@@ -158,7 +254,7 @@ export default function Trainings() {
               ))
             ) : (
               <TableRow>
-                <TableCell colSpan={6} className="text-center text-gray-500">
+                <TableCell colSpan={7} className="text-center text-gray-500">
                   Nenhum treinamento encontrado.
                 </TableCell>
               </TableRow>
@@ -169,3 +265,4 @@ export default function Trainings() {
     </Layout>
   );
 }
+
