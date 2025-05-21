1bf2ae264d2f92b75159669de27ec770f2b9072d
Lucas Augusto Kepler <lucaskepler991@gmail.com>
2025-05-21 11:09:39 -0300

    ajust fetch settings

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -20,7 +20,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:8000/users/login/", {
+      const response = await fetch("https://prochild-back-proud-star-4651.fly.dev/users/login/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
